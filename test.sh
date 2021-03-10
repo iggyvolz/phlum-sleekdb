bde3974ee7fb67915e1e2f507d6ff0ee83b8c14c
Katie Volz <iggyvolz@gmail.com>
2021-03-09 21:19:21 -0500

    Add config fiels for test

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-php -d auto_prepend_file=vendor/autoload.php genhelpers.php test/
+php -d auto_prepend_file=vendor/autoload.php genhelpers.php test/ vendor/iggyvolz/phlum/test/
 XDEBUG_MODE=coverage vendor/bin/phpunit
